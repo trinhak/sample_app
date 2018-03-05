c4599604cf193d532803ef75602882fdf8f89143
trinh <minhtrinhnguyendtu@gmail.com>
2018-03-05 16:14:25 +0700

    Chapter 10: Updating, showing, and deleting users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, length: {maximum: Settings.user.email_length},
     format: {with: VALID_EMAIL_REGEX}, presence: true, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: Settings.user.password_length}
+  validates :password, presence: true, length: {minimum: Settings.user.password_length},
+    allow_nil: true
   validates :name, presence: true, length: {maximum: Settings.user.name_length}
   before_save{email.downcase!}
   has_secure_password
