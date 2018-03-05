554a0608251cc8a3b7d70f01e5d77b000ad5c827
trinh <minhtrinhnguyendtu@gmail.com>
2018-03-05 16:14:25 +0700

    Chapter 9: Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,25 @@
 class SessionsController < ApplicationController
+  before_action :load_user, only: :create
+
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == Settings.user.remember ? remember(@user) : forget(@user)
+      redirect_to @user
     else
-      flash.now[:danger] = t ".danger"
+      flash.now[:danger] = t "sessions.new.danger"
       render :new
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  def load_user
+    @user = User.find_by email: params[:session][:email].downcase
   end
 end
