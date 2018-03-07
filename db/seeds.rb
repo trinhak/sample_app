82c357216ac02b7158c854a612e1f77493af2d11
trinh <minhtrinhnguyendtu@gmail.com>
2018-03-07 16:03:42 +0700

    Chapter 11: Account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,19 @@
-User.create!(name:  "Example User",
+User.create!(name: "Example User",
              email: "example@railstutorial.org",
-             password: "foobar",
-             password_confirmation: "foobar",
-             admin: true)
+             password: "abcde12345",
+             password_confirmation: "abcde12345",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name:  name,
+  User.create!(name: name,
                email: email,
                password: password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
